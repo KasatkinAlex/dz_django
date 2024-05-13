4d8dfc5a1f3ca118222517b73b54a67db84f4e22
Alex Kasatkin <akasatkin1990@mail.ru>
2024-05-13 18:32:51 +0400

    Задание 4, откатили миграцию, удалили файл с миграцией

--- catalog/models.py
+++ catalog/models.py
@@ -23,7 +23,6 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку', help_text='Введите цену продукта')
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     updated_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True)
-    manufactured_at = models.DateField(verbose_name='Дата производства продукта', null=True, blank=True)
 
     def __str__(self):
         return f'{self.name_product} {self.price}'
