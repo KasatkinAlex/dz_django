c7cb0d4736710ec93ffc64ac9428cdde1a8a1ed4
Alex Kasatkin <akasatkin1990@mail.ru>
2024-05-20 22:50:21 +0400

    ДЗ 20.2 вроде работает

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts
-
+from catalog.views import index, contacts, product_info
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name="index"),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('product_info/<int:pk>', product_info, name='product_info')
 ]
 
 if settings.DEBUG:
