d6c3b9f88204cecb7e9b5217c0dcf4ff3cd25242
Alex Kasatkin <akasatkin1990@mail.ru>
2024-06-03 23:11:07 +0400

    ДЗ 21.1 задание 3

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from pytils.templatetags.pytils_translit import slugify
 
 from catalog.models import Product, BlogPost
 
@@ -25,9 +26,20 @@ class ProductDetailView(DetailView):
 class BlogPostListView(ListView):
     model = BlogPost
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(publication_sign=True)
+        return queryset
+
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogPostCreateView(CreateView):
@@ -35,11 +47,28 @@ class BlogPostCreateView(CreateView):
     fields = ('title', 'content', 'image', 'publication_sign')
     success_url = reverse_lazy('catalog:blogpost')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+        return super().form_valid(form)
+
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
     fields = ('title', 'content', 'image', 'publication_sign')
     success_url = reverse_lazy('catalog:blogpost')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:blogpost_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogPostDeleteView(DeleteView):
