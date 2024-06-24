6b8e1e18ec5d1e2d9f84f5c3cb2c0db71a00c4b8
Alex Kasatkin <akasatkin1990@mail.ru>
2024-06-24 19:42:23 +0400

    ДЗ 22.1 формы

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product, BlogPost
+from catalog.models import Category, Product, BlogPost, VersionProduct
 
 
 @admin.register(Category)
@@ -19,3 +19,9 @@ class ProductAdmin(admin.ModelAdmin):
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'publication_sign', 'views_count')
     search_fields = ('title', 'content')
+
+
+@admin.register(VersionProduct)
+class VersionProductAdmin(admin.ModelAdmin):
+    list_display = ('version_number', 'version_title', 'version_activ')
+    search_fields = ('version_title', 'version_number', 'version_activ')
